80bd1714dc1bb9edc1dd6483cf09aeaaba3e04d9
pine-apple-labs <pineapplelabs.in@gmail.com>
2017-05-10 22:04:14 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<?php 
+include 'templates/head.php';
+echo "<body><div id=\"container\">";
+include 'templates/header.php';
+$pageCategory = "home";
+include 'templates/navigation.php';
+?>
+
+<div id="content">
+<div class="margin">
+<div style="float:left;width:500px;">
+<h1>Welcome to Pineapple Labs</h1>
+<h3>What we do?</h3>
+<p>
+We are involved in research and development activities
+for creating new experimental software products and services including mobile applications based on market needs. </p>
+<p>We are consultants, designers and developers of software and advisors to improve information systems and information technology solutions based on clients 
+requirement.
+ </p>
+ </div>
+ <div style="float:right;margin-top:100px;">
+ <img class="banner" name="slide" src="images/services.jpg" alt="image description"/>
+ </div>
+</div>
+</div>
+
+<?php include 'templates/footer.php';?>
+
+</div></body>
+</html>
